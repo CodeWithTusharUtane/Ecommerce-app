e4945037b3ac686824000889264b3dd7184b3c4c
Tushar Utane <89387408+CodeWitTushar@users.noreply.github.com>
2023-03-04 18:06:19 +0530

    Day: 8 Category Filter done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,77 +1,104 @@
-import { onAuthStateChanged } from 'firebase/auth'
-import { collection, getDocs } from 'firebase/firestore'
-import React, { useEffect, useState } from 'react'
-import {database, auth} from '../firebase/config'
-console.log("Tushar")
-const Cart = () => {
+import { onAuthStateChanged } from "firebase/auth";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { database, auth } from "../firebase/config";
 
-const [uid, setUid] = useState(null);
-const [cartData, setCartData] = useState([]);
-  onAuthStateChanged(auth, (user)=>{
-    if(user){
+console.log("Tushar");
+const Cart = () => {
+  const [count, setCount] = useState(1);
+  const [uid, setUid] = useState(null);
+  const [cartData, setCartData] = useState([]);
+  // console.log("first")
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
       setUid(user.uid);
     }
-})
+  });
 
-  const collectionName = "cart"+uid;
-  const productReference = collection(database, collectionName );
+  const collectionName = "cart" + uid;
+  const productReference = collection(database, collectionName);
 
-  useEffect(()=>{
-    const getData = async() => {
-      const data = await getDocs(productReference);
-      console.log(data)
-      setCartData(data.docs.map((item)=>({ ...item.data(),
-        id: item.id,})))
-    }
-    getData();
-  },[])
+  const getData = async () => {
+    const data = await getDocs(productReference);
+    // console.log(data)
+    setCartData(data.docs.map((item) => ({ ...item.data(), id: item.id })));
+  };
+  getData();
 
+  // const increaseCounter = () => {
+  //   setCount(count+1);
+
+  // }
+  // const reduceCounter = () => {
+  //   if(count < 1){
+  //     toast.error("Item cannot be 0")
+  //   }else{
+  //     setCount(count-1);
+  //   }
+  // }
+// console.log("first")
   return (
-    <div>
-      {
-        cartData.map((item)=>{
-          // console.log(items.Product)
-<div className="w-full h-10  text-2xl text-center bg-gray-300 ">
-          <h1>Cart Item's</h1>
-        </div>          
-          return(
-            <>
-              <div className="capitalize ">
-        
-        <div className="rounded-xl shadow-lg md:hover:scale-105 duration-300 mt-5 mb-6 md:w-[70%] lg:w-[50%] m-auto md:border-2 border-black/20 ">
-          <div className="md:flex items-center gap-10 justify-center   ">
-          <img src={item.Product.purl} alt="" className=" w-40 m-auto md:m-2 " />
-          <div className="md:ml-5">
-          <div className="text-center mt-2 text-xl md:text-2xl font-bold">Product 1</div>
-          <div className="text-center mt-2 text-xl md:text-2xl">Price- $5000</div>
-          <div className="text-center text-xl mt-2 md:text-2xl">Total- $50000</div>
-          <div className="text-center mt-2 text-xl md:text-2xl ">
-            <div>
-            Quantity <span  className="border-2 px-2 bg-gray-500 text-xl">+</span> <span>1</span> <span className="border-2 px-2 bg-gray-500 text-xl">-</span>
+    <div className="bg-[#e3dffd] text-[#2b3467]">
+    <div className="w-full h-10  text-2xl text-center  ">
+      <h1 className="pt-2 md:text-3xl">Cart Item's</h1>
+    </div>
+    {cartData.map((item) => {
+      // console.log(items.Product)
+      return (
+        <div className="capitalize ">
+          <div className="rounded-xl shadow-lg md:hover:scale-105 duration-300 mt-5 mb-6 md:w-[70%] lg:w-[50%] m-auto md:border-2 border-black/20 ">
+            <div
+              className="md:flex items-center gap-10 justify-center   "
+              key={item.Product.id}
+            >
+              <img
+                src={item.Product.purl}
+                alt=""
+                className=" w-40 m-auto md:m-2 "
+              />
+              <div className="md:ml-5">
+                <div className="text-center mt-2 text-xl md:text-2xl font-bold">
+                  {item.Product.pname}
+                </div>
+                <div className="text-center mt-2 text-xl md:text-2xl">
+                  Price- {item.Product.pprice}
+                </div>
+                <div className="text-center text-xl mt-2 md:text-2xl">
+                  Total- {item.Product.TotalProductPrice}
+                </div>
+                <div className="text-center mt-2 text-xl md:text-2xl ">
+                  <div>
+                    Quantity
+                    <button className="border-2 px-2 bg-[#2b3467] text-[#e3dffd] text-xl">
+                      +
+                    </button>
+                    <span>{count}</span>
+                    <button className="border-2 px-2 bg-[#2b3467] text-[#e3dffd] text-xl">
+                      -
+                    </button>
+                  </div>
+                  <button className="text-[#e3dffd] bg-[#2b3467] p-1 px-16 ml-5 mt-5 mb-5">
+                    Delete
+                  </button>
+                </div>
+              </div>
             </div>
-            <button className="bg-gray-500 text-white p-1 px-16 ml-5 mt-5 mb-5">Delete</button>
-          </div>
-          </div>
           </div>
         </div>
-        
-
-
-
-</div>
-            </>
-          )
-        })
-      }
-              <div className="mt-5  text-center bg-gray-300/50 p-2">
-          <span className="text-2xl font-bold">
-          Cart Total
-          </span>
-          <div className="mt-5 mb-5 text-2xl text-center ml-3">Sub Total- $55000</div>
-          <div className="m-2 p-2 text-2xl mt-3 bg-blue-500 text-white">Proceed to Checkout</div>   
-</div>
+      );
+    })}
+    <div className="mt-5  text-center  p-2">
+      <span className="text-2xl font-bold">Cart Total</span>
+      <div className="mt-5 mb-5 text-2xl text-center ml-3">
+        Sub Total- $55000
+      </div>
+      <div className="m-2 p-2 text-2xl mt-3 bg-blue-500 text-white">
+        Proceed to Checkout
+      </div>
     </div>
-  )
-}
+  </div>
+  );
+};
 
-export default Cart
+export default Cart;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,5 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { addDoc, collection, doc, getDocs, setDoc } from "firebase/firestore";
+import { collection, doc, getDocs, setDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -7,6 +7,7 @@ import { auth, database } from "../firebase/config";
 const Products = () => {
   const [productD, setProductD] = useState([]);
   const productRef = collection(database, "products");
+  const [select, setSelect] = useState();
 
   const navigate = useNavigate();
 
@@ -44,8 +45,8 @@ const Products = () => {
       Product["TotalProductPrice"] = Product.qty*prices;
       const productRef = collection(database, "cart"+uid);
       const citem = item.id;
+        toast.success("Added to Cart")
       // await addDoc(productRef, citem , {Product}).then(()=>{
-      //   toast.success("Added to Cart")
       //   // toast.success(item.id)
       // })
       
@@ -59,14 +60,25 @@ const Products = () => {
 
   return (
     <>
-      <div className="grid md:grid-cols-3 lg:grid-cols-4 ">
+
+    <div className="bg-[#E3DFFD] text-[#2B3467] text-center p-3">
+      <label htmlFor="category" className="font-bold text-xl mr-2">Category: </label>
+      <select name="cat" id="" value={select} onChange={(e)=>setSelect(e.target.value)} className="bg-[#2b3467] text-[#e3dffd] p-1 rounded-md text-center">
+        <option value="all">All</option>
+        <option value="watch">Watch</option>
+        <option value="headphone">Headphone</option>
+        <option value="neckband">Neckband</option>
+        <option value="buds">Wireless Buds</option>
+      </select>
+    </div>
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 bg-[#e3dffd] text-center">
         {productD.map((item) => {
-          
-          return (
-            <div
+          if(select === "all"){
+            return (
+              <div
               key={item.id}
-              className="border-4 border-black/10 m-5 text-center  p-2 shadow-lg rounded-lg hover:scale-105 duration-300"
-            >
+              className="border-4 border-black/10 m-5 text-center  p-2 shadow-lg rounded-lg hover:scale-105 duration-300 "
+              >
               <img src={item.purl} alt="" />
               <div className="h-1 w-full bg-black/10"></div>
               <div className="pt-3 text-2xl">{item.pname}</div>
@@ -78,6 +90,77 @@ const Products = () => {
               </div>
             </div>
           );
+        }else if(select === "watch" && item.pcat === "watch"){
+          return (
+            <div
+            key={item.id}
+            className="border-4 border-black/10 m-5 text-center  p-2 shadow-lg rounded-lg hover:scale-105 duration-300 "
+            >
+            <img src={item.purl} alt="" />
+            <div className="h-1 w-full bg-black/10"></div>
+            <div className="pt-3 text-2xl">{item.pname}</div>
+            <div className="pt-2">{item.pprice}</div>
+            <div className="pt-2 ">
+            <button className="bg-[#2b3467] text-[#e3dffd] p-1 mb-2 px-2 rounded-md" onClick={()=>AddToCart(item)}>
+              Add to Cart
+            </button>
+            </div>
+          </div>
+        );
+        }else if(select === "headphone" && item.pcat === "headphone"){
+          return (
+            <div
+            key={item.id}
+            className="border-4 border-black/10 m-5 text-center  p-2 shadow-lg rounded-lg hover:scale-105 duration-300 "
+            >
+            <img src={item.purl} alt="" />
+            <div className="h-1 w-full bg-black/10"></div>
+            <div className="pt-3 text-2xl">{item.pname}</div>
+            <div className="pt-2">{item.pprice}</div>
+            <div className="pt-2 ">
+            <button className="bg-[#2b3467] text-[#e3dffd] p-1 mb-2 px-2 rounded-md" onClick={()=>AddToCart(item)}>
+              Add to Cart
+            </button>
+            </div>
+          </div>
+        );
+        }else if(select === "neckband" && item.pcat === "neckband"){
+          return (
+            <div
+            key={item.id}
+            className="border-4 border-black/10 m-5 text-center  p-2 shadow-lg rounded-lg hover:scale-105 duration-300 "
+            >
+            <img src={item.purl} alt="" />
+            <div className="h-1 w-full bg-black/10"></div>
+            <div className="pt-3 text-2xl">{item.pname}</div>
+            <div className="pt-2">{item.pprice}</div>
+            <div className="pt-2 ">
+            <button className="bg-[#2b3467] text-[#e3dffd] p-1 mb-2 px-2 rounded-md" onClick={()=>AddToCart(item)}>
+              Add to Cart
+            </button>
+            </div>
+          </div>
+        );
+        }else if(select === "buds" && item.pcat === "bud"){
+          return (
+            <div
+            key={item.id}
+            className="border-4 border-black/10 m-5 text-center  p-2 shadow-lg rounded-lg hover:scale-105 duration-300 "
+            >
+            <img src={item.purl} alt="" />
+            <div className="h-1 w-full bg-black/10"></div>
+            <div className="pt-3 text-2xl">{item.pname}</div>
+            <div className="pt-2">{item.pprice}</div>
+            <div className="pt-2 ">
+            <button className="bg-[#2b3467] text-[#e3dffd] p-1 mb-2 px-2 rounded-md" onClick={()=>AddToCart(item)}>
+              Add to Cart
+            </button>
+            </div>
+          </div>
+        );
+        }
+
+
         })}
       </div>
     </>
