df122bd4e625cde8c0d403fd6dc8f57c740889ea
Tushar Utane <89387408+CodeWitTushar@users.noreply.github.com>
2023-03-07 20:20:39 +0530

    Added products to store functionality

--- src/components/Admin/AddProducts.jsx
+++ src/components/Admin/AddProducts.jsx
@@ -1,85 +1,154 @@
-import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
-import React, { useState } from 'react'
-import { toast } from 'react-toastify'
-import { storage } from '../../firebase/config'
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { database, storage } from "../../firebase/config";
 
-
-  const categories = [
-    {id: 1, name: "Watch"},
-    {id: 2, name: "Wireless Earphones"},
-    {id: 3, name: "NeckBands"},
-    {id: 4, name: "Headphones"},
-  ]
+const categories = [
+  { id: 1, name: "Watch" },
+  { id: 2, name: "Wireless Earphones" },
+  { id: 3, name: "NeckBands" },
+  { id: 4, name: "Headphones" },
+];
 
 const AddProducts = () => {
+  const [product, setProduct] = useState({
+    name: "",
+    imageUrl: "",
+    price: 0,
+    category: "",
+    brand: "",
+    desc: "",
+  });
 
-  const [product, setProduct] = useState({ name: "", imageUrl: "", price: 0, category: "", brand: "", desc: ""})
+  const navigate = useNavigate()
 
   const handleInputChange = (e) => {
-    const {name, value} = e.target
-    setProduct({...product, [name]: value})
-  }
+    const { name, value } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
   const handleImageChange = (e) => {
-    const file = e.target.files[0]
+    const file = e.target.files[0];
 
-    const storageRef = ref(storage, `images/${Date.now()}${file.name}`)
-    const uploadTask = uploadBytesResumable(storageRef, file)
+    const storageRef = ref(storage, `images/${Date.now()}${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on('state_changed',
-  () => {
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
-      setProduct({...product, imageUrl: downloadURL})
-      toast.success("Product uploaded successfully")
+    uploadTask.on("state_changed", () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        console.log("File available at", downloadURL);
+        setProduct({ ...product, imageUrl: downloadURL });
+        // toast.success("Image upload successfully");
+      });
     });
-  }
-);
-  }
+  };
 
   const addProduct = (e) => {
     e.preventDefault();
-    console.log("first:", product)
-  }
+    // console.log("first:", product);
+
+    try {
+      const docRef = addDoc(collection(database, "products"), {
+        name: product.name,
+        imageUrl: product.imageUrl,
+        price: Number(product.price),
+        category: product.category,
+        brand: product.brand,
+        desc: product.desc,
+        createdAt: Timestamp.now().toDate()
+      });
+      toast.success("Product added successfully")
+      navigate('/admin/all-products')
+    } catch (error) {
+      toast.error(error.message)
+    }
+  };
 
   return (
     <div>
       <h1>Add New Product</h1>
       <div>
         <form onSubmit={addProduct}>
-        <label>Product Name: </label>
-        <input type="text" placeholder='Product Name' required name='name' value={product.name} onChange={(e)=>handleInputChange(e)}/>
+          <label>Product Name: </label>
+          <input
+            type="text"
+            placeholder="Product Name"
+            required
+            name="name"
+            value={product.name}
+            onChange={(e) => handleInputChange(e)}
+          />
 
-        <label>Product Image: </label>
-        <input type="file" placeholder='Product Image' accept='image/*' name='image' onChange={(e)=> handleImageChange(e)}/>
-        <input type="text" name='imageUrl' value={product.imageUrl} disabled />
+          <label>Product Image: </label>
+          <input
+            type="file"
+            placeholder="Product Image"
+            accept="image/*"
+            name="image"
+            onChange={(e) => handleImageChange(e)}
+          />
+          <input
+            type="text"
+            name="imageUrl"
+            value={product.imageUrl}
+            disabled
+          />
 
-        <label>Product Price:</label>
-          <input type="number" required name='price' value={product.price} placeholder='Product Price' onChange={(e)=> handleInputChange(e)}/>
+          <label>Product Price:</label>
+          <input
+            type="number"
+            required
+            name="price"
+            value={product.price}
+            placeholder="Product Price"
+            onChange={(e) => handleInputChange(e)}
+          />
 
-        <label>Product Category:</label>
-          <select name="category" value={product.category} onChange={(e)=> handleInputChange(e)} required>
-
-            <option value="" disabled>--Choose The Product Category--</option>
-            {categories.map((item)=>{
-              return(
-                <option key={item.id} value={item.name}>{item.name}</option>
-              )
+          <label>Product Category:</label>
+          <select
+            name="category"
+            value={product.category}
+            onChange={(e) => handleInputChange(e)}
+            required
+          >
+            <option value="" disabled>
+              --Choose The Product Category--
+            </option>
+            {categories.map((item) => {
+              return (
+                <option key={item.id} value={item.name}>
+                  {item.name}
+                </option>
+              );
             })}
-
           </select>
 
-          <label >Product Brands</label>
-            <input type="text" placeholder='Product Brand' name='brand' value={product.brand} required  onChange={(e)=>handleInputChange(e)}/>
+          <label>Product Brands</label>
+          <input
+            type="text"
+            placeholder="Product Brand"
+            name="brand"
+            value={product.brand}
+            required
+            onChange={(e) => handleInputChange(e)}
+          />
 
-            <button>Add Product</button>
-          <label >Product Description: </label>
-            <textarea name="desc" value={product.desc} required cols="30" rows="10" onChange={(e)=>handleInputChange(e)} ></textarea>
+          <button>Add Product</button>
+          <label>Product Description: </label>
+          <textarea
+            name="desc"
+            value={product.desc}
+            required
+            cols="30"
+            rows="10"
+            onChange={(e) => handleInputChange(e)}
+          ></textarea>
 
-            <button>Add Product</button>
+          <button>Add Product</button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddProducts
+export default AddProducts;
--- src/components/Admin/ShowProduct.jsx
+++ src/components/Admin/ShowProduct.jsx
@@ -1,8 +1,131 @@
-import React from 'react'
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import {database, storage} from '../../firebase/config'
+import {FaEdit, FaTrashAlt} from 'react-icons/fa'
+import Loader from '../Loader';
+import { deleteObject, ref } from 'firebase/storage';
+import Notiflix from 'notiflix';
+import { useDispatch } from 'react-redux';
+import {STORE_PRODUCTS} from '../../redux/slice/productSlice'
 
 const ShowProduct = () => {
+
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+  
+
+  const getProducts = () => {
+    setIsLoading(true);
+
+    try{
+      const productsRef = collection(database, "products")
+      const q = query(productsRef, orderBy("createdAt", "desc"))
+
+      onSnapshot(q, (snapshot)=>{
+        // console.log(snapshot)
+
+        const allProductsData = snapshot.docs.map((doc)=>({
+          id: doc.id,
+          ...doc.data()
+        })) 
+        // console.log("products: ", allProductsData)
+        setProducts(allProductsData)
+        dispatch(STORE_PRODUCTS({
+          products: allProductsData
+        }))
+        setIsLoading(false)
+      })
+
+    }catch(error){
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  }
+
+  const confirmDelete = (id, imageUrl) => {
+    Notiflix.Confirm.show(
+      'Delete Product',
+      'Do you really want to delete this product?',
+      'Delete',
+      'Cancel',
+      function okCb() {
+        deleteProduct(id, imageUrl)
+      },
+      {
+        width: '320px',
+        borderRadius: '3px',
+        // etc...
+      },
+    );
+  }
+
+  const deleteProduct = async(id, imageUrl) => {
+    try { 
+      await deleteDoc(doc(database, "products", id));
+      const storageRef = ref(storage, imageUrl)
+      await deleteObject(storageRef);
+      toast.success("Successfully Deleted Product")
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   return (
-    <div>ShowProduct</div>
+    <>
+    {isLoading && <Loader/>}
+    <div className='w-full'>
+      <h2>All Products</h2> 
+      {products.length === 0 ? (
+        <p>
+          There is no product in Database.
+        </p>
+      ) : (
+        <table>
+          <thead>
+          <tr>
+            <th>Sr/no</th>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Price </th>
+            <th className='ml-9'>Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+          {
+            products.map((item, index)=>{
+              const {id, name, price, category, imageUrl} = item;
+              return (
+                
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td><img src={imageUrl} alt="" style={{width: "100px"}}/></td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>{`₹ ${price}`}</td>
+                    <td className='flex ml-9'>
+                      <Link o='/admin/add-products'><FaEdit size={20} color="green"/></Link>
+                      &nbsp;
+                        <FaTrashAlt size={18} color="red" onClick={() => confirmDelete(id, imageUrl)}/>
+                    </td>
+                  </tr>
+                
+              )
+            })
+          }
+          </tbody>
+        </table>
+      )}
+    </div>
+    </>
   )
 }
 
